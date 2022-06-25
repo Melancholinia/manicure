18d01338ced2c565d172077afa5d5580d538c62a
Mingkai Wang <mingkai@zju.edu.cn>
2022-06-26 01:08:47 +0800

    new

---
+++ js/middle.js
@@ -0,0 +1,50 @@
+$('#option-1').on('click', function (inevent) {
+    let gender = $(inevent.currentTarget)
+    $(gender).toggleClass('active');
+    $('#option-2').toggleClass('active');
+    if (($('#option-2').hasClass('active'))) {
+        $('#option-2').toggleClass('active');
+    }
+    if (($('#option-2').hasClass('active')) != ($('#option-1').hasClass('active'))) {
+        $('#confirm').addClass('active');
+    } else {
+        $('#confirm').removeClass('active');
+    }
+})
+
+$('#option-2').on('click', function (inevent) {
+    let gender = $(inevent.currentTarget)
+    $(gender).toggleClass('active');
+    if (($('#option-1').hasClass('active'))) {
+        $('#option-1').toggleClass('active');
+    }
+    if (($('#option-2').hasClass('active')) != ($('#option-1').hasClass('active'))) {
+        $('#confirm').addClass('active');
+        $('.slider2').removeAttr('disabled');
+    } else {
+        $('#confirm').removeClass('active');
+    }
+})
+
+$('#option-1').on('hover', function (inevent) {
+    let gender = $(inevent.currentTarget)
+    $(gender).toggleClass('active');
+    $('#option-2').toggleClass('active');
+    if (($('#option-2').hasClass('active'))) {
+        $('#option-2').toggleClass('active');
+    }
+})
+
+$('#option-2').on('hover', function (inevent) {
+    let gender = $(inevent.currentTarget)
+    $(gender).toggleClass('active');
+    if (($('#option-1').hasClass('active'))) {
+        $('#option-1').toggleClass('active');
+    }
+})
+
+$('').on('click', function (inevent) {
+    let value;
+})
+
+console.log($(--slide));
