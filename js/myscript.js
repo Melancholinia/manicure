712182ffafaff59d42d050dace7e11db9737c343
Mingkai Wang <mingkai@zju.edu.cn>
2022-06-25 18:52:12 +0800

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,10 @@
 var hands = ["../assets/images/my-hand.png", "../assets/images/bio-hand.png", "../assets/images/art-hand.png", "../assets/images/medi-hand.png", "../assets/images/robo-hand.png", "../assets/images/icon-hand.png"];
 var hand = document.getElementById("hand");
+var body = document.getElementsByTagName("body")[0];
 var index = 0;
 var timer = null;
 
-window.onload = function () {
+body.onload = function () {
     clearInterval(timer);
     timer = setInterval(function () {
         hand.style.backgroundImage = 'url(' + hands[index] + ')';
