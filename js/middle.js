1287b223d1a8a374cf8c99534e1e7032db902858
Mingkai Wang <mingkai@zju.edu.cn>
2022-06-26 17:40:01 +0800

    color!

--- js/middle.js
+++ js/middle.js
@@ -43,10 +43,25 @@ $('#option-2').on('hover', function (inevent) {
     }
 })
 
-$('.slider').on('change', function (inevent) {
+$('.slider').on('input propertychange', function (inevent) {
+    $('#choice').addClass('active');
     $('.slider2').removeAttr('disabled');
     $('#option-1').removeClass('active');
+    $('#confirm').addClass('active');
     $('#option-1').attr("disabled", true);
     $('#option-2').attr("disabled", true);
-    $('.slider').attr('active', true);
+    $('.slider').addClass('active');
+    var value = $('.slider')[0].value;
 })
+$('.slider').on('input propertychange', function (inevent) {
+    let value = $('.slider')[0].value;
+    document.documentElement.style.setProperty('--slider1', value);
+    $('.option').style.setProperty('--slider1', value);
+    $('#option-2').style.setProperty('--slider1', value);
+})
+var timer = null;
+clearInterval(timer);
+timer = setInterval(function () {
+    console.log(getComputedStyle(document.documentElement).getPropertyValue('--slider1'));
+    // console.log($('.slider').value);
+}, 300);
