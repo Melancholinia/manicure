94f4b3c4d30e6e7ed29f152ec870dd7f1c6f7528
Mingkai Wang <mingkai@zju.edu.cn>
2022-06-29 23:24:55 +0800

    new

--- js/fore.js
+++ js/fore.js
@@ -1,16 +1,28 @@
 const rows = document.querySelectorAll('ul li');
-var scrolled = 4.0;
+var scrolled = 10.0;
 var startY = 0.0, moveEndY = 0.0;
 var timer = null;
 var body = document.getElementsByTagName("body")[0];
 var windowHeight = $(window).height();
 body.style.height = windowHeight;
 
+body.onload = function () {
+    scrolled = 1.0;
+    for (let [index, row] of rows.entries()) {
+        let start = index * 5.0;
+        let end = (index + 2) * 5.0;
+        let progress = (scrolled - start) / (end - start);
+        if (progress >= 1) progress = 1;
+        if (progress <= 0) progress = 0;
+        row.style.setProperty('--progress', progress);
+        row.setAttribute('data-depth', progress);
+    }
+}
 document.addEventListener('mousewheel', function (e) {
-    scrolled += e.wheelDelta / 1000;
+    scrolled = scrolled - e.wheelDelta / 1000;
     console.log(scrolled);
     for (let [index, row] of rows.entries()) {
-        let start = index * 5.0 + 4.0;
+        let start = index * 5.0;
         let end = (index + 2) * 5.0;
         let progress = (scrolled - start) / (end - start);
         if (progress >= 1) progress = 1;
@@ -19,13 +31,6 @@ document.addEventListener('mousewheel', function (e) {
         row.setAttribute('data-depth', progress);
     }
 })
-
-body.onload = function () {
-    clearInterval(timer);
-    timer = setInterval(function () {
-        console.log(scrolled);
-    }, 1000);
-};
 
 body.on("touchstart", function (e) {
     e.preventDefault();
--- js/pinkie.js
+++ js/pinkie.js
@@ -1,3 +1,10 @@
+var body = document.getElementsByTagName("body")[0];
+body.onload = setTimeout(function () {
+    let newChat = document.createElement('div');
+    newChat.innerHTML = ' <div class="chat d-flex flex-row justify-content-start align-items-start"><div class="avatar"></div ><div class="bubble"><div>' + "你好" + '</div></div></div>';
+    let aa = document.getElementById("chatroom");
+    aa.innerHTML = aa.innerHTML + newChat.innerHTML;
+}, 1000);
 $('.input')[0].addEventListener('input', function () {
 
     this.style.height = this.scrollHeight + 'px';
@@ -17,7 +24,7 @@ $(".send-button").on('click', function (e) {
 
 function send() {
     let newChat = document.createElement('div');
-    newChat.innerHTML = ' <div class="chat d-flex flex-row-reverse justify-content-start align-items-start"><div class="avatar">头像</div ><div class="bubble"><div>' + $('.input')[0].value + '</div></div></div>';
+    newChat.innerHTML = ' <div class="chat d-flex flex-row-reverse justify-content-start align-items-start"><div class="avatar"></div ><div class="bubble"><div>' + $('.input')[0].value + '</div></div></div>';
     let aa = document.getElementById("chatroom");
     aa.innerHTML = aa.innerHTML + newChat.innerHTML;
     let inputCopy = $('.input')[0].value;
@@ -26,7 +33,7 @@ function send() {
     $('.add')[0].style.display = 'block';
     $('.send-button').attr("disabled", true);
     if (inputCopy.includes("爱")) {
-        inputCopy = "我不爱你";
+        inputCopy = "不，你不爱";
     } else if (inputCopy.includes("死")) {
         inputCopy = "会有那么一天";
     } else if (inputCopy.includes("孤独")) {
@@ -39,12 +46,40 @@ function send() {
         inputCopy = "我认真的";
     } else if (inputCopy.includes("金毛")) {
         inputCopy = "我不是金毛";
+    } else if (inputCopy.includes("他妈")) {
+        inputCopy = "去你妈";
+    } else if (inputCopy.includes("你好")) {
+        inputCopy = "好久不见";
+    } else if (inputCopy.includes("？")) {
+        inputCopy = "嗷呜";
+    } else if (inputCopy.includes("是的")) {
+        inputCopy = "不是的";
+    } else if (inputCopy.includes("不是的")) {
+        inputCopy = "是的";
+    } else if (inputCopy.includes("模仿")) {
+        inputCopy = "是你在模仿我！";
+    } else if (inputCopy.includes("没有")) {
+        inputCopy = "有";
+    } else if (inputCopy.includes("再骂")) {
+        inputCopy = "不想骂";
+    } else if (inputCopy.includes("嗯嗯")) {
+        inputCopy = "是什么事情呢？";
+    } else if (inputCopy.includes("确实")) {
+        inputCopy = "然而？";
+    } else if (inputCopy.includes("为什么")) {
+        inputCopy = "我也不知道";
+    } else if (inputCopy.includes("但是")) {
+        inputCopy = "...所以?";
+    } else if (inputCopy.includes("高兴")) {
+        inputCopy = "是的";
     }
+
     setTimeout(function () {
         let newChat = document.createElement('div');
-        newChat.innerHTML = ' <div class="chat d-flex flex-row justify-content-start align-items-start"><div class="avatar">头像</div ><div class="bubble"><div>' + inputCopy + '</div></div></div>';
+        newChat.innerHTML = ' <div class="chat d-flex flex-row justify-content-start align-items-start"><div class="avatar"></div ><div class="bubble"><div>' + inputCopy + '</div></div></div>';
         let aa = document.getElementById("chatroom");
-        aa.innerHTML = aa.innerHTML + newChat.innerHTML;
+        if (Math.random() > 0.4)
+            aa.innerHTML = aa.innerHTML + newChat.innerHTML;
     }, 1000);
 }
 document.onkeydown = function (e) {
